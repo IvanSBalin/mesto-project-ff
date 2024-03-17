bb60874fe554633b2c488734783f454b60671191
Ivan Balin <oopsrichnick@gmail.com>
2024-03-17 23:01:28 +0300

    feat:add global images links

---
+++ src/scripts/cards.js
@@ -0,0 +1,26 @@
+export const initialCards = [
+    {
+      name: "Chicago",
+      link: 'https://i.pinimg.com/originals/a9/a6/7c/a9a67cbaeaf784a01219dabf818dc2e9.jpg',
+    },
+    {
+      name: "Paris",
+      link: "https://i.pinimg.com/originals/44/d7/d7/44d7d768c833c815be051b3c6a4809a6.jpg",
+    },
+    {
+      name: "Tower bridge",
+      link: "https://i.pinimg.com/originals/21/15/71/2115710d9423dfda04c897d5102fd06c.jpg",
+    },
+    {
+      name: "Sydney",
+      link: "https://i.pinimg.com/originals/3c/55/6c/3c556c882363feaa0172b165ac712294.jpg",
+    },
+    {
+      name: "Cape of good hope",
+      link: "https://i.pinimg.com/originals/44/0c/75/440c755fd3a8519deccce6df466b3ca5.jpg",
+    },
+    {
+      name: "New Zealand",
+      link: "https://i.pinimg.com/originals/6c/49/df/6c49df07e48278b6c7bb5e4e1b1ffd1c.jpg",
+    }
+];
